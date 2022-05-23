23c79da0e480f43f315dd49b2f5dd4fbb4191c8c
InbarDanieli <inbar0308@gmail.com>
2022-05-23 16:39:44 +0300

    feat: devide card information

---
+++ src/components/CardsInformation.js
@@ -0,0 +1,33 @@
+export const cardsInformation = [
+  {
+    name: "inbar",
+    Key: 1,
+    flip: false
+  },
+  {
+    name: "inbar",
+    Key: 2,
+    flip: false
+  },
+  {
+    name: "omri",
+    Key: 3,
+    flip: false
+  },
+  {
+    name: "omri",
+    Key: 4,
+    flip: false
+  },
+  {
+    name: "plop",
+    Key: 5,
+    flip: false
+  },
+  {
+    name: "plop",
+    Key: 6,
+    flip: false
+  },
+  
+]
