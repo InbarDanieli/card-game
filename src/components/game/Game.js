b11b14bd451eafa12c23bcfce17445ee933c9135
InbarDanieli <inbar0308@gmail.com>
2022-11-09 10:25:26 +0200

    fix cards responsive

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -6,7 +6,7 @@ import { getCatsImages } from "../../services/CatAPI";
 import EndGame from "../EndGame/EndGame";
 
 
-function Game(props) {
+function Game() {
   const [twoCards, setTwoCards] = useState(false)
   const [info, setInfo] = useState({})
   const [score, setScore] = useState(0)
@@ -95,16 +95,16 @@ function Game(props) {
         />}
       <div style={{ opacity: endPage && 0.4 }} className="fullPageContainer">
         <div className="navbar" style={{ display: `${isLoading ? "none" : ""}` }}>
-          <div className={`${player === 0 ? "player choosen" : "player"}`}>Player One - {playerScores[0]}</div>
+          <div className={`${player === 0 ? "player choosen" : "player"}`}>P1 - {playerScores[0]}</div>
           <div className="buttonsContainer">
             <button card-amount="8" className={`cardsAmountButton ${cardAmount === 8 && "choosen"}`} onClick={() => { setReset(!isreset); setCardAmount(8) }}>8</button>
             <button card-amount="16" className={`cardsAmountButton ${cardAmount === 16 && "choosen"}`} onClick={() => { setReset(!isreset); setCardAmount(16) }}>16</button>
             <button card-amount="32" className={`cardsAmountButton ${cardAmount === 32 && "choosen"}`} onClick={() => { setReset(!isreset); setCardAmount(32) }}>32</button>
           </div>
-          <div className={`${player === 1 ? "player choosen" : "player"}`}>Player Two - {playerScores[1]}</div>
+          <div className={`${player === 1 ? "player choosen" : "player"}`}>P2 - {playerScores[1]}</div>
         </div>
         {isLoading && <div className="loading"><p>loading...</p></div>}
-        <div className="cards" style={{ "--colums": cards.length === 32 && 8 }}>
+        <div className="cards" style={{ "--colums": cards.length === 32 && 8, "--smaller-screen-colums": cards.length === 8 && 2 }}>
           {cards.map((info) =>
             <Card
               key={info.Key}
